aa16707ed2a19a86a5d4ed978a68bc6344cac449
namrahov <amrahovnurlan@gmail.com>
2022-02-08 20:01:28 +0400

    2nd commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/namrahov/klawpse
+
+go 1.15
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,47 @@
 package main
 
+import (
+	"github.com/jessevdk/go-flags"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
+)
 
-var ops struct {
+var opts struct {
 	Profile string `short:"p" long:"profile" default:"dev" description:"Application run profile"`
 }
 
 func main() {
 
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		panic(err)
+	}
 
+	initLogger()
+	initEnvVars()
 
+}
 
+func initLogger() {
+	log.SetLevel(log.InfoLevel)
+	if opts.Profile == "dev" {
+		log.SetFormatter(&log.JSONFormatter{})
+	}
+}
+
+func initEnvVars() {
+	if godotenv.Load("profiles/dev.env") != nil {
+		log.Fatal("Error in loading environment variables from: profiles/dev.env")
+	} else {
+		log.Info("Environment variables loaded from: profiles/dev.env")
+	}
+
+	if opts.Profile != "dev" {
+		profileFileName := "profiles/" + opts.Profile + ".env"
+		if godotenv.Overload(profileFileName) != nil {
+			log.Fatal("Error in loading environment variables from: ", profileFileName)
+		} else {
+			log.Info("Environment variables overloaded from: ", profileFileName)
+		}
+	}
 }
